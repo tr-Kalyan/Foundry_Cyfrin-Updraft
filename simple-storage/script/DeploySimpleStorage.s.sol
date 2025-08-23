408611094af7bd4fc30d8438d1bd0cee8ba30208
Kalyan_tr <kalyansde1@gmail.com>
2025-08-23 15:18:08 +0530

    first project

--- simple-storage/script/DeploySimpleStorage.s.sol
+++ simple-storage/script/DeploySimpleStorage.s.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
